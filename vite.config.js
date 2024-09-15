3e516ec66c93bd9c896769f5b928a48c28968149
Raiyan Ahmed <ray38250968@gmail.com>
2024-09-15 13:32:33 +0000

    chore: added env variable functionality

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://app:6969/",
-				changeOrigin: true,
-				secure: false,
+export default defineConfig(({ mode }) => {
+	const env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+	const API_URL = env.ISDOCKERIZED
+		? "http://localhost:6969/"
+		: "http://app:6969/";
+	const PORT = env.PORT ? env.PORT : 3000;
+	return {
+		plugins: [react()],
+		server: {
+			port: PORT,
+			proxy: {
+				"/api": {
+					target: API_URL,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-	},
+	};
 });
