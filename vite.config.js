635929cdf458308364f094ead7c31b4063c71b48
Raiyan Ahmed <ray38250968@gmail.com>
2024-09-18 15:52:24 +0000

    config : edit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
 	const env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 	const API_URL = env.API_URL;
+	console.log("API_URL : ", API_URL);
 	const PORT = env.PORT ? env.PORT : 3000;
 	return {
 		plugins: [react()],
