3b99d6425a22113bf328ad592d6daf5a14b58cb7
Raiyan Ahmed <ray38250968@gmail.com>
2024-10-02 17:25:43 +0000

    progress: adding image uploading options

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,47 +1,55 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import Blog from "../models/blogModel.js";
+import multer from "multer";
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // @desc Blog Post
 // route POST /api/blog/
 // @access Protected
-const postBlog = asyncHandler(async (req, res) => {
-	console.log("Adding blog..."); // TODO : REMOVE THIS
-	try {
-		const blogData = req.body.blog;
-		if (!blogData) {
-			res.status(400);
-			throw new Error("Invalid blog data");
-		} else if (!blogData.title || !blogData.content) {
-			res.status(400);
-			throw new Error("Invalid blog data : Title and Content are required");
+const postBlog = asyncHandler((req, res, next) => {
+	upload.single("image")(req, res, async (err) => {
+		console.log("Adding blog..."); // TODO : REMOVE THIS
+		try {
+			const blogData = req.body.blog;
+			if (!blogData) {
+				res.status(400);
+				throw new Error("Invalid blog data");
+			} else if (!blogData.title || !blogData.content) {
+				res.status(400);
+				throw new Error("Invalid blog data : Title and Content are required");
+			}
+			console.log("Blog Data:", blogData); // TODO : REMOVE THIS
+			console.log("req.Users :", req.user); // TODO : REMOVE THIS
+			const user = await User.findOne({
+				email: req.user.email,
+			});
+			const blog = new Blog({
+				title: blogData.title,
+				content: blogData.content,
+				author: req.user._id,
+				tags: blogData.tags,
+				category: blogData.category,
+			});
+			const createdBlog = await blog.save();
+			res.status(201).json({
+				_id: createdBlog._id,
+				title: createdBlog.title,
+				content: createdBlog.content,
+				tags: createdBlog.tags,
+				category: createdBlog.category,
+			});
+		} catch (error) {
+			res.status(401);
+			console.log("Error:", error.message);
+			throw new Error("Something went wrong...");
 		}
-		console.log("Blog Data:", blogData); // TODO : REMOVE THIS
-		console.log("req.Users :", req.user); // TODO : REMOVE THIS
-		const user = await User.findOne({
-			email: req.user.email,
-		});
-		const blog = new Blog({
-			title: blogData.title,
-			content: blogData.content,
-			author: req.user._id,
-			tags: blogData.tags,
-			category: blogData.category,
-		});
-		const createdBlog = await blog.save();
-		res.status(201).json({
-			_id: createdBlog._id,
-			title: createdBlog.title,
-			content: createdBlog.content,
-			tags: createdBlog.tags,
-			category: createdBlog.category,
-		});
-	} catch (error) {
-		res.status(401);
-		console.log("Error:", error.message);
-		throw new Error("Something went wrong...");
 	}
-});
+	)
+}
+
+);
 
 const getBlog = asyncHandler(async (req, res) => {
 	try {
@@ -86,4 +94,5 @@ const getBlogs = asyncHandler(async (req, res) => {
 	}
 });
 
-export { postBlog, getBlogs, getBlog };
+
+export { postBlog, getBlogs, getBlog, upload };
